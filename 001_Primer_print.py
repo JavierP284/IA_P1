a8c8205d5f5ee356216d289a22bb0229290fa6cc
JavierP284 <a22310284@ceti.mx>
2025-02-14 21:13:53 -0600

    Primer print

--- 001_Primer_print.py
+++ 001_Primer_print.py
@@ -1,7 +1,8 @@
+#Imprimir texo usando 
 print("¡Hola Mundo!")
-print()
+print()#print para que pase un renglon completo
 print("Hisssssssss....")
 print()
 print("Miau!")
 print()
-print("Inteligencia \nArttificial")
+print("Inteligencia \nArttificial")#usar \n para que pase de renglon
