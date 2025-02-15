1a8a8fcaf76bd3fd72455613b7bb952e9b489839
JavierP284 <a22310284@ceti.mx>
2025-02-14 23:50:53 -0600

    Variables

---
+++ 007_Variables.py
@@ -0,0 +1,24 @@
+#Variables
+var = 1 #Se les da un nombre a la variable y su contenido
+account_balance = 1000.0
+client_name = 'John Doe'
+
+print("Imprimir Variables")
+print(var, account_balance, client_name) #Al llamar las variables mostraran lo que tengan guardado
+
+num = "3"
+print("\nNumero: " + num) #Se pueden convinar tecto con variables mientras sean del mismo tipo de dato.
+
+print("\nAsignar nuevo Valor a la Variable")
+print("Valor original: ", end="")
+print(var)
+var = var+1 #Se le puede dar un nuevo valor a la variable
+print("var+1= ", end="")
+print(var)
+
+var = 100
+print("Var= ", end="")
+print(var)
+var= 200+300 #Incluso se le puede dar un valor completamente nuevo y con una operacion
+print("Var= 200 + 300 = ",end="")
+print(var)
