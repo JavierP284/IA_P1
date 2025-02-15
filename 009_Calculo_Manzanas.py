eafcd0ade5db7b1855a0a0eb84af124e8e8f60ac
JavierP284 <a22310284@ceti.mx>
2025-02-15 00:02:54 -0600

    Calculo Manzanas

---
+++ 009_Calculo_Manzanas.py
@@ -0,0 +1,10 @@
+#Se declaran las variables de las manzanas de cada quien
+john = 2
+mary = 4
+adam = 3
+
+#Se imprimen el numero de manzanas que tiene cada uno
+print("Manzanas de John:", john,"\nManzanas de Mary:", mary, "\nManzanas de Adam:", adam)
+
+total_apples= john+mary+adam #se declara la nueva variable que contiene la suma de las manzanas
+print("Total de manzanas:", total_apples) #se imprime el total de manzanas
