84a28dbc0a2ed5d2648ba55afa6dd4c1d4af5c08
JavierP284 <a22310284@ceti.mx>
2025-02-14 22:02:31 -0600

    Tipos de datos

---
+++ 004_Tipos_de_datos.py
@@ -0,0 +1,30 @@
+#Lierales
+print("Literales")
+print("2")
+print(2)
+
+#Enteros
+print("\nEnteros")
+print(11111111)
+print(11_111_111) #Se pueden agregar guiones bajos para escribirlos mas facilmente
+print(0o123) #83 en Octal (0o octal)
+print(0x123) #Hexadecimal (0x hexadecimal)
+
+#Flotantes
+print("\nFlotantes")
+print(0.4)
+print(4.0)
+print(3E8) #Notacion cientifica para escribir un numero muy grande
+print(6.62607E-34) #Si es muy pequeño o muy grande se va a imprimir con la notacion mas corta
+
+#Cadenas
+print("\nCadenas")
+print("Me gusta \"Monty Python\"") #Se debe de utilizar caracter de escape como \ para usar comillas denttro de comidas o caracteres que puedan modificar el codigo
+print('Me gusta "Monty Python"')
+print("I'm Monty Python.")
+print('I\'m Monty Python.')
+
+#Booleanos
+print("\nBooleanos")
+print(True > False) #Se pueden usar elemenos para definir un valor sobre otro
+print(True < False)
