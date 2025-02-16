bb74fa44392d241940bbf9af839ef773901a192e
JavierP284 <a22310284@ceti.mx>
2025-02-15 23:33:51 -0600

    Operadores y funciones

---
+++ 018_Operadores_y_funciones.py
@@ -0,0 +1,4 @@
+#Codigo para calcular una expresion
+x=float(input("Ingresa el valor para x: ")) #Solicitar valor para x
+y= 1/(x+(1/(x+(1/(x+(1/x)))))) #Calculo de la expresion sustituyendo x
+print("y =", y) #Mostrar el resultado
