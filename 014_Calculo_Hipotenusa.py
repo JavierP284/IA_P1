fe75940f6424f0f56a6d48d16cb25dc2b87169fa
JavierP284 <a22310284@ceti.mx>
2025-02-15 22:44:48 -0600

    Calculo Hipotenusa

---
+++ 014_Calculo_Hipotenusa.py
@@ -0,0 +1,5 @@
+#Calcular Hipotenusa
+leg_a = float(input("Ingresa la longitud del primer cateto: "))#Soliicitar el primer cateto
+leg_b = float(input("Ingresa la longitud del segundo cateto: "))#Solicitar el segundo cateto
+hypo = (leg_a**2 + leg_b**2) ** .5 #Funcion 
+print("La longitud de la hipotenusa es:", hypo) #Mostrar resultado1
