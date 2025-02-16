bd1ada52c85312cbcf475233584338d490911b07
JavierP284 <a22310284@ceti.mx>
2025-02-16 17:57:09 -0600

    Operadores 2

---
+++ 020_Operadores_2.py
@@ -0,0 +1,27 @@
+#Operador de igualdad
+print("\nOperador de Igualdad")
+var = 0  # Asignando 0 a var
+print("La variable es igual a 0: " ,var == 0) #Hacer la pregunta de si es verdadero o falso
+var = 1 # Asignando 1 a var
+print("El valor de la variable cambia a 1")
+print("La variable es igual a 0: " ,var == 0)  #Hacer la pregunta de si es verdadero o falso
+
+#Operador de desigualdad
+print("\nOperador de Desigualdad")
+print("La variable es igual a 0")
+var = 0  # Asignando 0 a var
+print("La variable es diferente a 0: " ,var != 0) #Hacer la pregunta de si la variable es diferente a 0 con !=
+print("Se cambia el valor de la variable a 1")
+var = 1  # Asignando 1 a var
+print("La variable es diferente a 0:" ,var != 0) 
+
+#Operador mayor que y mayor o igual que
+print("\nMayor que y Mayor o igual que")
+black_sheep=5
+white_sheep=7
+print("Ovejas negras = 5")
+print("Ovejas blancas = 7")
+print(black_sheep > white_sheep)  # Mayor que
+print(black_sheep >= white_sheep) # Mayor o igual que
+print(black_sheep < white_sheep)  # Mayor que
+print(black_sheep <= white_sheep) # Mayor o igual que
