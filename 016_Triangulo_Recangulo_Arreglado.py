2beaf4d0a3739b4c13cc3cbc91aeee93bb8e87b2
JavierP284 <a22310284@ceti.mx>
2025-02-15 23:06:48 -0600

    Triangulo Rectangulo

---
+++ 016_Triangulo_Recangulo_Arreglado.py
@@ -0,0 +1,4 @@
+#Calcular triangulo rectangulo
+leg_a = float(input("Ingresa la longitud del primer cateto: "))
+leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+print("La longitud de la hipotenusa es " + str((leg_a**2 + leg_b**2) ** .5)) #Utilizar str para pasar el resultado entero como una cadena
