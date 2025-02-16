52f337bde883f44ef89bd3abf42ac494840460b7
JavierP284 <a22310284@ceti.mx>
2025-02-15 23:17:46 -0600

    Calculos con variables

---
+++ 017_Calculos_con_variables.py
@@ -0,0 +1,8 @@
+#Realizar calculos con 2 valores solicitados
+a=float(input("Ingresa el primer valor: ")) #Solicitar primer valor
+b=float(input("Ingresa el segundo valor: ")) #Solicitar segundo valor
+print("La suma de las variables es: " ,a+b) #Mostrar la suma
+print("La resta de las variables es: " ,a-b) #Mostrar la resta
+print("La multiplicacion de las variables es: " ,a*b) #Mostrar la multiplicacion
+print("La division de las variables es: " ,a/b) #Mostrar la division
+print("\nEso es todo, amigos!")
