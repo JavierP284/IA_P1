743773a3d436dedb3cf179e24fa1cd70b03e1b2a
JavierP284 <a22310284@ceti.mx>
2025-02-16 18:54:39 -0600

    Sentencia if-else

---
+++ 023_Sentencia_if-else.py
@@ -0,0 +1,8 @@
+entrada = input("Ingresa el nombre de una planta: ") #Solicitar Nombre de una planta
+
+if entrada == "espatifilo":
+    print("No, ¡quiero un gran Espatifilo!") #Si se usa espatifilo mostrar
+elif entrada == "Espatifilo":
+    print ("Si, ¡El Espatifilo! es la mejor planta de todos los tiempos!") #Si se usa Espatifilo usar
+else:
+    print("¡Espatifilo!, !No" ,entrada + "!") #Cualquier otra planta
