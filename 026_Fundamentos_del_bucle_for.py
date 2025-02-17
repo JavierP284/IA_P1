4e8cd487f56802caa40aa93212a758388114841c
JavierP284 <a22310284@ceti.mx>
2025-02-16 19:37:40 -0600

    Fundamentos del bucle for

---
+++ 026_Fundamentos_del_bucle_for.py
@@ -0,0 +1,7 @@
+import time
+
+for i in range (1,6): #Se da el rango del conteo para que sea de 1 a 5
+    print(f"{i} Mississippi") #Se imprime el contteo con Mississipi
+    time.sleep(1) #Se termina el conteo y el bucle
+
+print("Listos o no, aqui voy!") #Se imprime el mensaje final
