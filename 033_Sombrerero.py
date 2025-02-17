7818b7deb3f1d8ccb33c438a38f32e26ad767e53
JavierP284 <143145196+JavierP284@users.noreply.github.com>
2025-02-17 13:04:48 -0600

    Sombrerero

---
+++ 033_Sombrerero.py
@@ -0,0 +1,11 @@
+hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+print("\nLista original: " ,hat_list)
+
+hat_list [2] = int(input("Introduce un nuevo numero: ")) #Solicitar un nuevo numero para reemplazar el numero de en medio
+
+hat_list.pop() #Eliminar el ultimo elemento de la lista del hat_list[4] tambien era opcion
+
+print("Longitud de la lista: ", len(hat_list)) # Imprimir la longitud de la lista existente.
+
+print("Lista actualizada: " ,hat_list) #Imprimir la lista actualizada
