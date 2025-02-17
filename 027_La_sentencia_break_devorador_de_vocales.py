47852abd946f948f744e50b4674929e340e1df43
JavierP284 <a22310284@ceti.mx>
2025-02-16 19:44:42 -0600

    La sentencia break devorador de vocales

---
+++ 027_La_sentencia_break_devorador_de_vocales.py
@@ -0,0 +1,5 @@
+while True:  # Bucle infinito hasta que se cumpla la condición
+    palabra = input("Ingresa una palabra: ")
+    if palabra == "chupacabra":  # Condición para salir del bucle
+        print("Has dejado el bucle con éxito.")
+        break  # Rompe el bucle si la palabra es "chupacabra"
