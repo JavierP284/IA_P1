08655a44e63c2944c3389eadd363e0f1886c79ac
JavierP284 <143145196+JavierP284@users.noreply.github.com>
2025-02-18 11:37:22 -0600

    Operacion listas

--- 036_Operacion_listas.py
+++ 036_Operacion_listas.py
@@ -1,9 +1,9 @@
-my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9] #Declaracion de la lista
 unique_list = []
 
 for num in my_list:
-    if num not in unique_list:
+    if num not in unique_list: #Funcion para eliminar elementos repetidos
         unique_list.append(num)
 
-print("La lista con elementos únicos:")
+print("La lista con elementos únicos:") #Imprimir la lista sin numeros repetidos
 print(unique_list)
