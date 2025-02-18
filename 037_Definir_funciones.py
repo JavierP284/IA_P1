5aeed9629ec2493ff440b53d445030801a3c85f7
JavierP284 <143145196+JavierP284@users.noreply.github.com>
2025-02-18 11:56:13 -0600

    Definir funciones

---
+++ 037_Definir_funciones.py
@@ -0,0 +1,7 @@
+def message(): #Declarar la función
+    valor=int(input("Ingresar valor: ")) #Colocar el funconamiento
+    print("Tu valor es:" ,valor)
+
+print("Inicia aqui.")
+message() #Llamar a la función
+print("Termina aqui.")
