febbfecd42b2600569fdeb1f544ed464fa95c04f
JavierP284 <143145196+JavierP284@users.noreply.github.com>
2025-02-18 11:37:09 -0600

    Ordenamiento

--- 035_Ordenamiento.py
+++ 035_Ordenamiento.py
@@ -1,9 +1,10 @@
+#Declarar la lista
 my_list = []
 swapped = True
-num = int(input("¿Cuántos elementos deseas ordenar?: "))
+num = int(input("¿Cuántos elementos deseas ordenar?: ")) #Solicitar el numero de elementos que se agregaran a la lista
 
 for i in range(num):
-    val = float(input("Ingresa un elemento de la lista: "))
+    val = float(input("Ingresa un elemento de la lista: ")) #Solicitar los valores dentro de un bucle para llenar el numero solicitado
     my_list.append(val)
 
 while swapped:
@@ -11,7 +12,7 @@ while swapped:
     for i in range(len(my_list) - 1):
         if my_list[i] > my_list[i + 1]:
             swapped = True
-            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i] #Funcion para ordenar los numeros
 
-print("\nOrdenada:")
+print("\nOrdenada:") #Mostrar la lista ordenada
 print(my_list)
