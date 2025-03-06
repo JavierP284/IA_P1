ed4787c04bad3008011e8bf5809085d6bd5dd3ff
JavierP284 <a22310284@ceti.mx>
2025-03-05 19:09:34 -0600

    Multiples Argumentos Clave

--- 002_Multiples_Argumentos_Clave.py
+++ 002_Multiples_Argumentos_Clave.py
@@ -1,7 +1,7 @@
-#Utilizar argumentos dentro de print
-print("Mi nombre es", "Python.", end="") #Utilizar end para que coloque un espacio entre esta linea y la siguiente
+# Utilizar argumentos dentro de print
+print("Mi nombre es", "Python.", end="") # Utilizar end para que coloque un espacio entre esta linea y la siguiente
 print("Monty Python.")
-print("Mi", "nombre", "es", "Javier", "Pérez", sep="-") #Utilizar sep para agregar - entre cada argumento que imprime esta función
+print("Mi", "nombre", "es", "Javier", "Pérez", sep="-") # Utilizar sep para agregar - entre cada argumento que imprime esta función
 print()
-print("Mi", "nombre", "es", sep="_", end="/") #Mezcla de sep y end
+print("Mi", "nombre", "es", sep="_", end="/") # Mezcla de sep y end
 print("Javier", "Pérez.", sep="-", end="/\n")
